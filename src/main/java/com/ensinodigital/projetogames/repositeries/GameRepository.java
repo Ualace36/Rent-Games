e1057eb8bf654e45be2c06a6fb0a5a10f595e5b1
Ualace36 <ualace26@gmail.com>
2023-07-12 20:41:56 -0300

    TODO usar query no filtro

--- src/main/java/com/ensinodigital/projetogames/repositeries/GameRepository.java
+++ src/main/java/com/ensinodigital/projetogames/repositeries/GameRepository.java
@@ -9,7 +9,7 @@ import com.ensinodigital.projetogames.entities.Game;
 import com.ensinodigital.projetogames.projections.GameMinProjection;
 
 public interface GameRepository extends JpaRepository<Game, Long>{
-
+    //TODO NÃO USEI O JPQL
 	@Query(nativeQuery = true, value = """
 			SELECT tb_game.id, tb_game.title, tb_game.game_year AS `year`, tb_game.img_url AS imgUrl,
 			tb_game.short_description AS shortDescription, tb_belonging.position
